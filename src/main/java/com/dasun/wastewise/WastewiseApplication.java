a5397a04fa7cbb64a776f19ec1738c445b427603
Dasun Jayatunga <jayatungadasun04@gmail.com>
2026-05-06 03:16:46 +0530

    Initial commit

---
+++ src/main/java/com/dasun/wastewise/WastewiseApplication.java
@@ -0,0 +1,13 @@
+package com.dasun.wastewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastewiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasun/wastewise/WastewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasun.wastewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
